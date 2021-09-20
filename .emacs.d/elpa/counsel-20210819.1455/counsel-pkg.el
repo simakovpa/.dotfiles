273acd04ed9bd6c42f0beba2dc58157d19d2c8fd
Pavel Simakov <toli4@outlook.com>
2021-09-21 00:00:28 +0700

    .emacs - added custom agenda views

---
+++ .emacs.d/elpa/counsel-20210819.1455/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from counsel.el  -*- no-byte-compile: t -*-
+(define-package "counsel" "20210819.1455" "Various completion functions using Ivy" '((emacs "24.5") (ivy "0.13.4") (swiper "0.13.4")) :commit "9baa4422d4cfc32d527e85e37ccaa5abd7491a05" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :keywords '("convenience" "matching" "tools") :url "https://github.com/abo-abo/swiper")
--- .emacs.d/org-clock-save.el
+++ .emacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - toli4-manjaro at <2021-09-19 Вс 20:03>
+;; org-clock-save.el - toli4-manjaro at <2021-09-20 Пн 23:36>
 (setq org-clock-stored-history '(("/home/toli4/Dropbox/emacsOrgFiles/refile.org" . 20) ("/home/toli4/Dropbox/emacsOrgFiles/refile.org_archive" . 863)))
